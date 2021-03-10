ed0e542c8fe343b42887486e4411be96961ea3f5
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-03-10 16:27:37 -0300

    added flag to enable/disable object detection

--- src/camera_capture.py
+++ src/camera_capture.py
@@ -10,22 +10,24 @@ import numpy as np
 
 from picamera import PiCamera
 
-# from object_detection import detect_common_objects
-
 
 # ----------------------------------------------------------------------------------------------- #
 # Variables and config
 # ----------------------------------------------------------------------------------------------- #
 MODE = os.environ.get("MODE", "picamera")
-RES_WIDTH = os.environ.get("RES_WIDTH", "640")
-RES_HEIGHT = os.environ.get("RES_HEIGHT", "480")
-FRAMERATE = os.environ.get("FRAMERATE", "24")
+RES_WIDTH = int(os.environ.get("RES_WIDTH", 640))
+RES_HEIGHT = int(os.environ.get("RES_HEIGHT", 480))
+FRAMERATE = int(os.environ.get("FRAMERATE", 24))
+DETECT = bool(int(os.environ.get("DETECT", 1)))
+
+if DETECT:
+    from object_detection import detect_common_objects
 
 # Camera settings
-camera_resolution_width = int(RES_WIDTH)
-camera_resolution_height = int(RES_HEIGHT)
+camera_resolution_width = RES_WIDTH
+camera_resolution_height = RES_HEIGHT
 camera_resolution = (camera_resolution_width, camera_resolution_height)
-camera_framerate = int(FRAMERATE)
+camera_framerate = FRAMERATE
 camera_rotation = 180
 camera_exposure_mode = "auto"
 camera_sensor_mode = 0
@@ -39,10 +41,14 @@ camera_drc_strength = "high"
 class CameraCapture:
     """Camera Capture"""
 
+    frame = None
+
     def __init__(self, mode=MODE):
         """Init the programm using picamera or opencv backend"""
 
         self.mode = mode
+        self.is_picamera = False
+        self.is_opencv = False
         if self.mode.lower() == "picamera":
             self.is_picamera = True
             self.camera = PiCamera()
@@ -60,8 +66,8 @@ class CameraCapture:
             sleep(2)
             if not self.camera.isOpened():
                 raise IOError("Could not open Camera")
-            self.camera.set(CAP_PROP_FRAME_WIDTH, camera_resolution_width)
-            self.camera.set(CAP_PROP_FRAME_HEIGHT, camera_resolution_height)
+            self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, camera_resolution_width)
+            self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_resolution_height)
         else:
             raise ValueError("Camera mode unknown. Please select either picamera or opencv")
  
@@ -82,22 +88,17 @@ class CameraCapture:
         """
 
         if self.is_picamera:
-            image_array = np.empty(
+            CameraCapture.frame = np.empty(
                 (camera_resolution_height * camera_resolution_width * 3,),
                 dtype=np.uint8
             )
-            self.camera.capture(image_array, "bgr")
-            image_array = image_array.reshape((camera_resolution_height, camera_resolution_width, 3))
-            # Toggle the comments to enable/disable object detection
-            # image = detect_common_objects(image_array)
-            image = image_array
-            return cv2.imencode('.jpg', image)[1].tobytes()
+            self.camera.capture(CameraCapture.frame , "bgr")
+            CameraCapture.frame = CameraCapture.frame.reshape((camera_resolution_height, camera_resolution_width, 3))
 
         if self.is_opencv:
             if self.camera.isOpened():
-                _, frame = self.camera.read()
-                rotated_frame = cv2.rotate(frame, cv2.ROTATE_180)
-                # Toggle the comments to enable/disable object detection
-                # image = detect_common_objects(rotated_frame)
-                image = rotated_frame
-                return cv2.imencode('.jpg', image)[1].tobytes()
+                _, CameraCapture.frame = self.camera.read()
+                CameraCapture.frame = cv2.rotate(CameraCapture.frame, cv2.ROTATE_180)
+        
+        image = detect_common_objects(CameraCapture.frame) if DETECT else CameraCapture.frame
+        return cv2.imencode('.jpg', image)[1].tobytes()
